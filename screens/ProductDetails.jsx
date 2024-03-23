1fde27af58a62f9bee0d228ccb6c3dac8ff7a681
Durga Potnuru <vijaypotnuru123@gmail.com>
2024-03-23 18:47:21 +0530

    2:10:59

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,6 +1,6 @@
 import { View, Text, SafeAreaView, TouchableOpacity, Image } from "react-native";
 import React from "react";
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, SimpleLineIcons } from "@expo/vector-icons";
 import styles from "./productDetails.style";
 import { COLORS } from "../constants";
 
@@ -12,13 +12,36 @@ const ProductDetails = ({ navigation }) => {
           <TouchableOpacity onPress={() => navigation.goBack()}>
             <Ionicons name="chevron-back-circle" size={30} />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => navigation.goBack()}>
+          <TouchableOpacity onPress={() => {}}>
             <Ionicons name="heart" size={30} color={COLORS.primary} />
           </TouchableOpacity>
         </View>
         <Image source={{ uri: "https://res.cloudinary.com/dmlhm8dwi/image/upload/v1655812677/cld-sample-4.jpg" }} style={styles.image} />
         <View style={styles.details}>
+          <View style={styles.titleRow}>
+            <Text style={styles.title}>Product</Text>
+            <View style={styles.priceWrapper}>
+              <Text style={styles.price}>₹ 660.88</Text>
+            </View>
+          </View>
 
+          <View style={styles.ratingRow}>
+            <View style={styles.rating}>
+              {[1, 2, 3, 4, 5].map((index) => (
+                <Ionicons key={index} name="star" size={24} color="gold" />
+              ))}
+              <Text style={styles.ratingText}> (4.9)</Text>
+            </View>
+            <View style={styles.rating}>
+              <TouchableOpacity onPress={() => {}}>
+                <SimpleLineIcons name="plus" size={20}  />
+              </TouchableOpacity>
+              <Text style={styles.ratingText}> (4.9)</Text>
+              <TouchableOpacity onPress={() => {}}>
+                <SimpleLineIcons name="minus" size={20} />
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
       </View>
     </SafeAreaView>
--- screens/productDetails.style.js
+++ screens/productDetails.style.js
@@ -8,6 +8,7 @@ const styles = StyleSheet.create({
     },
     container: {
         flex: 1,
+        backgroundColor: COLORS.lightWhite,
 
     },
     upperRow: {
@@ -28,7 +29,54 @@ const styles = StyleSheet.create({
     },
     details: {
         marginTop: -SIZES.large,
+        backgroundColor: COLORS.lightWhite,
+        width: SIZES.width,
+        borderTopLeftRadius: SIZES.medium,
+        borderTopRightRadius: SIZES.medium,
+    },
+    titleRow: {
+        marginHorizontal: 20,
+        paddingBottom: SIZES.small,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: SIZES.width - 44,
+        top: 20,
+    },
+    ratingRow: {
+        paddingBottom: SIZES.small,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: SIZES.width - 10,
+        top: 5,
+    },
+    rating: {
+        top: SIZES.large,
+        flexDirection: "row",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        marginHorizontal: SIZES.large,
+    },
+    ratingText: {
+        color: COLORS.gray,
+        fontFamily: "medium",
+    },
+    title: {
+        fontFamily: "bold",
+        fontSize: SIZES.large,
+
+    },
+    price: {
+        paddingHorizontal: 10,
+        fontFamily: "semibold",
+        fontSize: SIZES.large,
     }
+    ,
+    priceWrapper: {
+        backgroundColor: COLORS.secondary,
+        borderRadius: SIZES.large,
+    },
 
 });
 
