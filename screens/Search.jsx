5f6a8e16fcd9e3efd903135c7cfedc055d7a5740
Durga Potnuru <vijaypotnuru123@gmail.com>
2024-04-02 17:51:02 +0530

    Add search functionality to Search component (3:44:55)

--- screens/Search.jsx
+++ screens/Search.jsx
@@ -1,10 +1,21 @@
 import { TouchableOpacity, TextInput, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import styles from "./search.style";
 import { Feather, Ionicons } from "@expo/vector-icons";
 import { COLORS, SIZES } from "../constants";
 
 const Search = () => {
+  const [searchKey, setSearchKey] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearch = () => {
+   try {
+    
+   } catch (error) {
+    
+   }
+  };
+
   return (
     <SafeAreaView style={styles.safeareaview}>
       <View style={styles.searchContainer}>
@@ -12,10 +23,10 @@ const Search = () => {
           <Ionicons name="camera-outline" size={SIZES.xLarge} style={styles.searchIcon} />
         </TouchableOpacity>
         <View style={styles.searchWrapper}>
-          <TextInput style={styles.searchInput} value="" onPressIn={() => {}} placeholder="What are you looking for" />
+          <TextInput style={styles.searchInput} value={searchKey} onChangeText={setSearchKey} placeholder="What are you looking for" />
         </View>
         <View>
-          <TouchableOpacity style={styles.searchBtn}>
+          <TouchableOpacity style={styles.searchBtn} onPress={() => handleSearch()}>
             <Feather name="search" size={24} color={COLORS.offwhite} />
           </TouchableOpacity>
         </View>
